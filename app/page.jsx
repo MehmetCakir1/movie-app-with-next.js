e913bf6eea5d49c55a7016c3382e6f48212de8ea
MehmetCakir1 <mehmetcakir4227@gmail.com>
2023-07-13 15:46:44 +0300

    movie app with next.js

---
+++ app/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='bg-red-500 text-blue-700 text-5xl text-center p-6 font-bold'>Home</div>
+  )
+}
+
+export default Home
